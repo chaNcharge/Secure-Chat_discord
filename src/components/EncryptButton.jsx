aa49686f3565c7f51738b002de3cc3038d9fc863
chaNcharge <chancharge@gmail.com>
2024-03-05 11:22:12 -0800

    Fix private key import, cleanup

--- src/components/EncryptButton.jsx
+++ src/components/EncryptButton.jsx
@@ -12,6 +12,7 @@ export default function EncryptButton() {
         const userIdModule = BdApi.Webpack.getModule(BdApi.Webpack.Filters.byKeys("getCurrentUser"));
         let channelId = SelectedChannelStore.getChannelId();
         let userId = userIdModule.getCurrentUser().id;
+        let message = "";
         function TextInput(props) {
             return <textarea
                 placeholder={props.placeholder || "Enter plaintext to encrypt"}
@@ -34,21 +35,26 @@ export default function EncryptButton() {
                 placeholder="Enter encrypted text..."
                 onChange={event => {
                     console.log(event.target.value);
-                    (async () => {
-                        // TODO: only run this block of code if PRIVATE-<channel-id>.key is NOT present in pluginDirectory, non-private key is encrypted
-                        const encryptedAESkey = fs.readFileSync(`${pluginDirectory}/${channelId}.key`, 'utf8');
-                        const privKeyFile = fs.readFileSync(`${pluginDirectory}/private-${userId}.pem`, 'utf8');
-                        const privKey = await importStringToKey(privKeyFile, 'private');
-                        const decryptedAESkey = await decryptAESKey(encryptedAESkey, privKey)
-                        
-                        const encryptedText = await encryptMessage(); //TODO: complete this with key file parsing with channel-id.key
-                    })();
+                    message = event.target.value;
                 }}
             />,
             {
                 confirmText: "Send",
                 cancelText: "Cancel",
-                onConfirm: () => console.log("Pressed Send, send encrypted message here"),
+                onConfirm: () => {
+                    console.log(`Pressed Send, send encrypted message here: ${message}`);
+                    (async () => {
+                        // TODO: only run this block of code if PRIVATE-<channel-id>.key is NOT present in pluginDirectory, non-private key is encrypted
+                        const encryptedAESkey = fs.readFileSync(`${pluginDirectory}/${channelId}.key`, 'utf8');
+                        const privKeyFile = fs.readFileSync(`${pluginDirectory}/private-${userId}.pem`, 'utf8');
+                        const privKey = await importStringToKey(privKeyFile, 'private');
+                        console.log("private key imported");
+                        const decryptedAESkey = await decryptAESKey(encryptedAESkey, privKey)
+                        console.log(decryptedAESkey);
+
+                        //const encryptedText = await encryptMessage(); //TODO: complete this with key file parsing with channel-id.key
+                    })();
+                },
                 onCancel: () => console.log("Pressed 'Cancel' or escape")
             }
         );
--- src/lib/RSAKeyCreation.js
+++ src/lib/RSAKeyCreation.js
@@ -31,17 +31,17 @@ export async function createKeyPair(modulusLength) {
  * @returns {Promise<string>} A promise resolving to a string encoded in base64 of the key object
  */
 export async function exportKeyToString(key, keyType) {
-    if (keyType === "private") {
-        const exported = await window.crypto.subtle.exportKey("pkcs8", key);
-        const exportedAsString = ab2str(exported);
-        const exportedAsBase64 = window.btoa(exportedAsString);
-        const pemExported = `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`;
-        return pemExported;
-    } else {
+    if (keyType === "public") {
         const exported = await window.crypto.subtle.exportKey("spki", key);
         const exportedAsString = ab2str(exported);
         const exportedAsBase64 = window.btoa(exportedAsString);
         const pemExported = `-----BEGIN PUBLIC KEY-----\n${exportedAsBase64}\n-----END PUBLIC KEY-----`;
+        return pemExported;
+    } else {
+        const exported = await window.crypto.subtle.exportKey("pkcs8", key);
+        const exportedAsString = ab2str(exported);
+        const exportedAsBase64 = window.btoa(exportedAsString);
+        const pemExported = `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`;
         return pemExported;
     }
 }
@@ -53,51 +53,26 @@ export async function exportKeyToString(key, keyType) {
  * @returns {Promise<CryptoKey>} A promise resolving to a CryptoKey object
  */
 export async function importStringToKey(pem, keyType) {
-    if (keyType === "public") {
-        // fetch the part of the PEM string between header and footer
-        const pemHeader = "-----BEGIN PUBLIC KEY-----";
-        const pemFooter = "-----END PUBLIC KEY-----";
-        const pemContents = pem.substring(
-            pemHeader.length,
-            pem.length - pemFooter.length - 1,
-        );
-        // base64 decode the string to get the binary data
-        const binaryDerString = window.atob(pemContents);
-        // convert from a binary string to an ArrayBuffer
-        const binaryDer = str2ab(binaryDerString);
+    // fetch the part of the PEM string between header and footer
+    const pemHeader = keyType === "public" ? "-----BEGIN PUBLIC KEY-----" : "-----BEGIN PRIVATE KEY-----";
+    const pemFooter = keyType === "public" ? "-----END PUBLIC KEY-----" : "-----END PRIVATE KEY-----";
+    const pemContents = pem.substring(
+        pemHeader.length,
+        pem.length - pemFooter.length - 1,
+    );
+    // base64 decode the string to get the binary data
+    const binaryDerString = window.atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
 
-        return window.crypto.subtle.importKey(
-            "spki",
-            binaryDer,
-            {
-                name: "RSA-OAEP",
-                hash: "SHA-256",
-            },
-            true,
-            ["encrypt"],
-        );
-    } else {
-        // fetch the part of the PEM string between header and footer
-        const pemHeader = "-----BEGIN PRIVATE KEY-----";
-        const pemFooter = "-----END PRIVATE KEY-----";
-        const pemContents = pem.substring(
-            pemHeader.length,
-            pem.length - pemFooter.length - 1,
-        );
-        // base64 decode the string to get the binary data
-        const binaryDerString = window.atob(pemContents);
-        // convert from a binary string to an ArrayBuffer
-        const binaryDer = str2ab(binaryDerString);
-
-        return window.crypto.subtle.importKey(
-            "pkcs8",
-            binaryDer,
-            {
-                name: "RSA-PSS",
-                hash: "SHA-256",
-            },
-            true,
-            ["decrypt"],
-        );
-    }
+    return window.crypto.subtle.importKey(
+        keyType === "public" ? "spki" : "pkcs8",
+        binaryDer,
+        {
+            name: "RSA-OAEP",
+            hash: "SHA-256",
+        },
+        true,
+        keyType === "public" ? ["encrypt"] : ["decrypt"]
+    );
 }
