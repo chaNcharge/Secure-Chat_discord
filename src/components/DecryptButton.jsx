b6c1f593d3cc24d95b8b08b15b0af3d52969f40e
chaNcharge <chancharge@gmail.com>
2024-03-05 19:28:11 -0800

    Add decrypt button, finish message encryption/decryption

--- src/components/DecryptButton.jsx
+++ src/components/DecryptButton.jsx
@@ -1,9 +1,9 @@
 import fs from "fs";
-import { encryptMessage } from "../lib/hybridencrypt";
+import { decryptMessage } from "../lib/hybridencrypt";
 import { decryptAESKey, importAESKey } from "../lib/AESKey";
 import { importStringToKey } from "../lib/RSAKeyCreation";
 
-export default function EncryptButton() {
+export default function DecryptButton() {
     const pluginDirectory = BdApi.Plugins.folder + "/SecureChat";
 
     function handleClick() {
@@ -13,10 +13,10 @@ export default function EncryptButton() {
         const userIdModule = BdApi.Webpack.getModule(BdApi.Webpack.Filters.byKeys("getCurrentUser"));
         let channelId = SelectedChannelStore.getChannelId();
         let userId = userIdModule.getCurrentUser().id;
-        let message = "";
+        let ciphertextStr = "";
         function TextInput(props) {
             return <textarea
-                placeholder={props.placeholder || "Enter plaintext to encrypt"}
+                placeholder={props.placeholder || "Paste ciphertext JSON to decrypt"}
                 onChange={props?.onChange}
             />;
         }
@@ -31,24 +31,25 @@ export default function EncryptButton() {
         }
 
         BdApi.UI.showConfirmationModal(
-            "Encrypt Text: Make sure you have already exchanged AES keys first!",
+            "Decrypt Text: Make sure you have already exchanged AES keys first!",
             <TextInput
-                placeholder="Enter plaintext to encrypt..."
+                placeholder="Paste ciphertext JSON to decrypt"
                 onChange={event => {
-                    message = event.target.value;
+                    ciphertextStr = event.target.value;
                 }}
             />,
             {
-                confirmText: "Send",
+                confirmText: "Decrypt",
                 cancelText: "Cancel",
                 onConfirm: () => {
+                    const ciphertextObj = JSON.parse(ciphertextStr);
                     (async () => {
                         // only run this block of code if PRIVATE-<channel-id>.key is NOT present in pluginDirectory, non-private key is encrypted
                         if (fs.existsSync(`${pluginDirectory}/PRIVATE-${channelId}.key`)) {
                             const aesKeyFile = fs.readFileSync(`${pluginDirectory}/PRIVATE-${channelId}.key`, 'utf8');
                             const aesKey = await importAESKey(aesKeyFile);
-                            const encryptedTextObj = await encryptMessage(aesKey, message);
-                            sendMessage("```" + JSON.stringify(encryptedTextObj) + "```");
+                            const decryptedText = await decryptMessage(aesKey, ciphertextObj.ciphertext, ciphertextObj.iv);
+                            BdApi.UI.alert("Decrypted Text", decryptedText);
                         } else {
                             // Runs when no unencrypted aes key exists, aka you received an encrypted key and can decrypt it with YOUR private key
                             console.log("Decrypting AES key");
@@ -57,8 +58,8 @@ export default function EncryptButton() {
                             const privKey = await importStringToKey(privKeyFile, 'private');
                             const decryptedAESkey = await decryptAESKey(encryptedAESkey, privKey)
 
-                            const encryptedTextObj = await encryptMessage(decryptedAESkey, message);
-                            sendMessage("```" + JSON.stringify(encryptedTextObj) + "```");
+                            const decryptedText = await decryptMessage(decryptedAESkey, ciphertextObj.ciphertext, ciphertextObj.iv);
+                            BdApi.UI.alert("Decrypted text", decryptedText);
                         }
                     })();
                 },
@@ -69,7 +70,7 @@ export default function EncryptButton() {
 
     return (
         <button className="my-component" onClick={handleClick}>
-            Encrypt
+            Decrypt
         </button>
     )
 }
